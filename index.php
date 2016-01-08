c7a8c4fa06e96059398abcd37f55b3dc1d411ae1
Cameron Stenmark <cameron.stenmark@iss-i.ca>
2016-01-07 17:11:04 -0800

    First Tic Tac Toe logic

--- index.php
+++ index.php
@@ -7,20 +7,70 @@
     </head>
     <body>
         <?php
-            $temp = 'Jim';
-            echo 'Hi, my name is ';
-            echo $temp;
-            $temp = 'geek';
-            echo "I am a ";
-            echo $temp;
-            $temp = 10;
-            echo 'My level is ';
-            echo $temp;
+            $position = $_GET['board'];
+            $squares = str_split($positions);
             
+            if(winner('x', $squares)) 
+            {
+                echo 'You win.';
+            } else if (winner('o', $squares)) 
+            {
+                echo 'I win.';
+            } else 
+            {
+                echo 'No winner yet.';
+            }
         ?>
     </body>
 </html>
-
+<?php
+function winner($token, $position) {
+        $won = false;
+        if(($position[0] == $token) &&
+           ($position[1] == $token) &&
+           ($position[2] == $token)) 
+        {
+            $won = true;
+        } else if(($position[3] == $token) &&
+           ($position[4] == $token) &&
+           ($position[5] == $token)) 
+        {
+            $won = true;
+        } else if (($position[6] == $token) &&
+           ($position[7] == $token) &&
+           ($position[8] == $token)) 
+        {
+            $won = true;
+        } else if (($position[0] == $token) &&
+           ($position[3] == $token) &&
+           ($position[6] == $token)) 
+        {
+            $won = true;
+        } else if (($position[1] == $token) &&
+           ($position[4] == $token) &&
+           ($position[7] == $token)) 
+        {
+            $won = true;
+        } else if (($position[2] == $token) &&
+           ($position[5] == $token) &&
+           ($position[8] == $token)) 
+        {
+            $won = true;
+        } else if (($position[0] == $token) &&
+           ($position[4] == $token) &&
+           ($position[8] == $token)) 
+        {
+            $won = true;
+        } else if (($position[2] == $token) &&
+           ($position[4] == $token) &&
+           ($position[6] == $token)) 
+        {
+            $won = true;
+        }
+        
+        return $won;
+    }
+?>
 
 
 
